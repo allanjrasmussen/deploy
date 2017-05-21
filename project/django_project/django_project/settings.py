2e2c43b48dbbe88a428c4830685674694ef9028f
allanjrasmussen <ajr@onetec.dk>
2017-05-21 17:39:33 +0200

    Add files via upload

--- project/django_project/django_project/settings.py
+++ project/django_project/django_project/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'da'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
